b7185323147a06130861d8fdf3623eca16ea976f
humakasaat <hkasaat@hotmail.com>
2022-12-20 17:53:33 -0500

    all about students

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+     SyntaxtStudents ahmad= new SyntaxtStudents();
+
+        System.out.println("ahmad is hardworking");
+    }
+}
---
+++ src/SyntaxtStudents.java
@@ -0,0 +1,8 @@
+public class SyntaxtStudents {
+    String FirstName;
+    String lastName;
+    String email;
+    int grades;
+
+
+}
